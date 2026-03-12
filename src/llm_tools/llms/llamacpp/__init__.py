9567552c33a944dbdfafb27f06c93d114fbeb331
Thomas Kloiber <thomas.kloiber@leftshift.one>
2026-03-12 13:31:17 +0100

    feat(llm): reorganize package exports

---
+++ src/llm_tools/llms/llamacpp/__init__.py
@@ -0,0 +1,7 @@
+from llm_tools.llms.llamacpp.llama_cpp_llm import LlamaCppLLM, LlamaCppLlmSettings
+
+
+__all__ = [
+    "LlamaCppLLM",
+    "LlamaCppLlmSettings",
+]
--- src/llm_tools/llms/openai/__init__.py
+++ src/llm_tools/llms/openai/__init__.py
@@ -1,8 +1,6 @@
-from llm_tools.llms.llamacpp.llama_cpp_llm import LlamaCppLLM
 from llm_tools.llms.openai.azure_openai_llm import AzureOpenAiLLM
 
 
 __all__ = [
     "AzureOpenAiLLM",
-    "LlamaCppLLM",
 ]
